82602ad03355c8703799b873bdff8f1d54ceb267
Till W <theasuro@gmail.com>
2017-04-22 15:42:57 +0200

    world stuff

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    [SerializeField] private MeshFilter meshFilter;
+    [SerializeField] private float distortionScale = 1f;
+    [SerializeField] private float frequency = 1f;
+    [SerializeField] private bool update = false;
+    [SerializeField] private Color waterColor;
+    [SerializeField] private Color groundColor;
+
+    private Mesh originalMesh;
+
+    private void Awake()
+    {
+        originalMesh = new Mesh();
+        originalMesh.SetVertices(meshFilter.mesh.vertices.ToList());
+        originalMesh.SetIndices(meshFilter.mesh.GetIndices(0), MeshTopology.Triangles, 0);
+        originalMesh.SetNormals(meshFilter.mesh.normals.ToList());
+        originalMesh.uv = meshFilter.mesh.uv;
+        Regenerate();
+    }
+
+    private void Update()
+    {
+        if (update)
+        {
+            update = false;
+            Regenerate();
+        }
+    }
+
+    private void Regenerate()
+    {
+        List<Vector3> newVertexPositions = new List<Vector3>(originalMesh.vertexCount);
+        List<Color> newVertexColors = new List<Color>();
+        for (int i = 0; i < originalMesh.vertexCount; i++)
+        {
+            Vector2 texPos = originalMesh.uv[i];
+            Vector3 vertexPos = originalMesh.vertices[i];
+
+            // hack to hide seam
+            float seamScale = 1f;
+            if (texPos.x <= 0.1f)
+                seamScale *= texPos.x * 10f;
+            if (texPos.x >= 0.9f)
+                seamScale *= (1 - texPos.x) * 10f;
+            if (texPos.y <= 0.1f)
+                seamScale *= texPos.y * 10f;
+            if (texPos.y >= 0.1f)
+                seamScale *= (1 - texPos.y) * 10f;
+
+            newVertexPositions.Add(vertexPos);
+            newVertexColors.Add(Color.blue);
+
+            Vector3 vertexNormal = originalMesh.normals[i];
+            float noiseValue = Mathf.PerlinNoise(texPos.x * frequency, texPos.y * frequency) * seamScale;
+            newVertexPositions.Add(vertexPos + noiseValue * distortionScale * vertexNormal);
+            newVertexColors.Add(Color.Lerp(waterColor, groundColor, noiseValue));
+        }
+        meshFilter.mesh.SetVertices(newVertexPositions);
+        meshFilter.mesh.SetColors(newVertexColors);
+    }
+}
---
+++ Assets/Shaders/VertexColors.shader
@@ -0,0 +1,47 @@
+﻿Shader "Unlit/earth"
+{
+	Properties
+	{
+
+	}
+	SubShader
+	{
+		Tags { "RenderType"="Opaque" }
+		LOD 100
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			
+			#include "UnityCG.cginc"
+
+			struct VertIn
+			{
+				float4 vertex : POSITION;
+				float4 color : COLOR;
+			};
+
+			struct VertOut
+			{
+				float4 color : COLOR;
+				float4 vertex : SV_POSITION;
+			};
+			
+			VertOut vert (VertIn v)
+			{
+				VertOut o;
+				o.color = v.color;
+				o.vertex = UnityObjectToClipPos(v.vertex);
+				return o;
+			}
+			
+			fixed4 frag (VertOut i) : SV_Target
+			{
+				return fixed4(i.color);
+			}
+			ENDCG
+		}
+	}
+}
